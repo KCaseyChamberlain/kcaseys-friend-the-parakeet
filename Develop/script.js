ca47813081430402942c23163add396c01b2126d
KCaseyChamberlain <caseygchamberlain@gmail.com>
2022-01-08 15:40:03 -0700

    JavaScript DONE!

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,92 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+// Targets the HTML IDs
+var lowercaseElement = document.querySelector("#lowercase");
+var uppercaseElement = document.querySelector("#uppercase");
+var numbersElement = document.querySelector("#numbers");
+var symbolsElement = document.querySelector("#symbols");
+var lengthElement = document.querySelector("#passlength");
+var passwordBox = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
+var hiddenElement = document.querySelector("#hidden-criteria");
+var criteriaButton = document.getElementById("unhide");
+var hiddenCriteria = document.getElementById("hidden-criteria");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+// Hides cirteriaButton and shows hiddenCriteria
+function showHiddenCriteria() {
+hiddenCriteria.style.display = "block";
+criteriaButton.style.display = "none";
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Function that places password in password box.
+function generatePassword(){
+var length = returnLength();
+var newPassword = "";
+  for(var i = 0; i < length; i++) {
+  newPassword += randomize();
+  }
+passwordBox.value = newPassword;
+}
+
+// Randomizer
+function randomize(){
+  var criteriaArray = [];
+
+  if(uppercaseElement.checked){
+    criteriaArray.push(returnUppercase);
+  }
+
+  if(lowercaseElement.checked){
+    criteriaArray.push(returnLowercase);
+  }
+
+  if(numbersElement.checked){
+    criteriaArray.push(returnNumbers);
+  }
+
+  if(symbolsElement.checked){
+    criteriaArray.push(returnSymbols)
+  }
+
+  console.log(criteriaArray)
+    var randomCharacter = criteriaArray[Math.floor(Math.random()*criteriaArray.length)];
+    return randomCharacter.call();
+  }
+
+
+
+// Start of selectable criteria functions
+function returnLowercase() {
+var lower = 'abcdefghijklmnopqrstuvwxyz';
+var lowerCharacter = lower[Math.floor(Math.random()*lower.length)];
+return lowerCharacter;
+}
+
+function returnUppercase() {
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var upperCharacter = upper[Math.floor(Math.random()*upper.length)];
+return upperCharacter;
+}
+
+function returnNumbers() {
+var number = "0123456789";
+var numberCharacter = number[Math.floor(Math.random()*number.length)];
+return numberCharacter;
+}
+
+function returnSymbols() {
+var symbol = "!@#$%^&*()?";
+var symbolCharacter = symbol[Math.floor(Math.random()*symbol.length)];
+return symbolCharacter;
+}
+
+
+
+function returnLength() {
+var length = lengthElement.value;
+return length;
+}
+
+
+
+criteriaButton.addEventListener("click", showHiddenCriteria);
+generateBtn.addEventListener("click", generatePassword);
